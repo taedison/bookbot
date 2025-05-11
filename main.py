23a63e46cb1214f689e3b2e01068b68167d4d6f4
taedison <taedison@posteo.de>
2025-05-11 12:11:47 +0200

    count words

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ def get_book_text(file_path):
         file_contents = f.read()
     return file_contents
 
+def count_words(text):
+    text_list = text.split()
+    text_num = 0
+    for words in text_list:
+        text_num += 1
+    return text_num
+
 def main():
     booktext = get_book_text("books/frankenstein.txt")
-    print(booktext)
+    num_words = count_words(booktext)
+    print(f"{num_words} words found in the document")
 
 main()
